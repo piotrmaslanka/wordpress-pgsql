2152dc00693308b34e3a22a6e24292d47c0516b5
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2023-04-11 20:50:26 +0200

    he wanted 6.2, he got 6.2

--- wp-content/pg4wp/core.php
+++ wp-content/pg4wp/core.php
@@ -31,7 +31,7 @@ $replaces = array(
 );
 // Ensure class uses the replaced mysql_ functions rather than mysqli_
 define( 'WP_USE_EXT_MYSQL', true);
-eval( str_replace( array_keys($replaces), array_values($replaces), file_get_contents(ABSPATH.'/wp-includes/wp-db.php')));
+eval( str_replace( array_keys($replaces), array_values($replaces), file_get_contents(ABSPATH.'/wp-includes/class-wpdb.php')));
 
 // Create wpdb object if not already done
 if (! isset($wpdb) && defined('DB_USER'))
